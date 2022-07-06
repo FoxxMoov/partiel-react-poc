16a8eff6a6053df40d593c4be6cbd97d0afff1b6
corentin.ravet <corentin.ravet@jellyfish.com>
2022-07-06 17:27:14 +0200

    :tada: (First commit )

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div id='edo-donate-btn'>
+        <a href='https://www.every.org/lilbubsbigfund/donate'>Donate</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
