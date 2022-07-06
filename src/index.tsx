758679295d79b79a2cfdf6cd036b3adc33552d97
corentin.ravet <corentin.ravet@jellyfish.com>
2022-07-06 17:36:45 +0200

    update:

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,27 @@ import reportWebVitals from './reportWebVitals';
 
 const base = document.getElementById('root') as HTMLElement;
 
-if (base.hasChildNodes()) {
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />);
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// if (base.hasChildNodes()) {
+//   const root = ReactDOM.createRoot(
+//     document.getElementById('root') as HTMLElement
+//   );
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// } else {
+//   ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />);
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
